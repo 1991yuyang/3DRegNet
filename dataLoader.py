44882c0e6d58f3e5b3e65b09bb15a792a8634dca
yuyang <1230-jj@163.com>
2022-05-05 14:51:38 +0800

    modify dataLoader.py

--- dataLoader.py
+++ dataLoader.py
@@ -3,12 +3,22 @@ import torch as t
 from torch import nn
 import open3d as o3d
 import numpy as np
+from numpy import random as rd
+from colors import COLOR_MAP
 
 
 class MySet(data.Dataset):
 
-    def __init__(self, voxel_size):
+    def __init__(self, voxel_size, R_range, t_range):
+        """
+
+        :param voxel_size: voxel size for downsample one point cloud
+        :param R_range: value range of item of rotation matrix
+        :param t_range: value range of item of translate vector
+        """
         self.voxel_size = voxel_size
+        self.R_range = R_range
+        self.t_range = t_range
 
     def __getitem__(self, index):
         pass
@@ -36,20 +46,38 @@ class MySet(data.Dataset):
         return pcd_down, pcd_fpfh
 
     def pcd2tensor(self, pcd):
-        print(type(pcd.points))
-        pcd_tensor = t.tensor(pcd.points)
+        points = np.asarray(pcd.points)
+        pcd_tensor = t.tensor(points).type(t.FloatTensor)
         return pcd_tensor
 
-    def show_pcd(self, pcd):
-        pcd.paint_uniform_color([1, 0.706, 0])
-        o3d.visualization.draw_geometries([pcd], "pcd")
+    def show_pcd(self, pcd_list):
+        for i, pcd in enumerate(pcd_list):
+            pcd.paint_uniform_color(COLOR_MAP[40 - i])
+        o3d.visualization.draw_geometries(pcd_list, "pcd")
+
+    def random_generate_R_t(self):
+        random_lie_param = rd.uniform(self.R_range[0], self.R_range[1], (3, 1))
+        R = t.tensor(o3d.geometry.get_rotation_matrix_from_axis_angle(random_lie_param)).type(t.FloatTensor)  # rotation matrix, centroid is the center of rotation
+        t_vec = t.tensor(rd.uniform(self.t_range[0], self.t_range[1], 3)).type(t.FloatTensor)
+        return R, t_vec
+
+    def rotate_pcd(self, pcd, R):
+        pcd_after_rotate = pcd.rotate(R)
+        return pcd_after_rotate
+
+    def translate_pcd(self, pcd, t):
+        pcd_after_translate = pcd.translate(t)
+        return pcd_after_translate
 
 
 if __name__ == "__main__":
+    from copy import deepcopy
     pcd_pth = r"F:\python_project\test_open3d\cloud_image_00000.ply"
-    s = MySet(0.001)
+    s = MySet(0.001, [-10, 10], [-10, 10])
     pcd = s.load_one_pc(pcd_pth)
     pcd, fpfh = s.preprocess_point_cloud(pcd)
-    s.show_pcd(pcd)
-    pcd_tensor = s.pcd2tensor(pcd)
-    print(pcd_tensor)
+    pcd_orig = deepcopy(pcd)
+    R, t_vec = s.random_generate_R_t()
+    pcd_after_rotation = s.rotate_pcd(pcd, R)
+    pcd_after_translate = s.translate_pcd(pcd_after_rotation, t_vec)
+    s.show_pcd([pcd_orig, pcd_after_translate])

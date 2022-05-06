3e5985e07a857e1bf7474bf20d6a88ee86301b08
yuyang <1230-jj@163.com>
2022-05-06 18:06:01 +0800

    modify dataLoader.py

--- dataLoader.py
+++ dataLoader.py
@@ -22,7 +22,7 @@ valid_data_dir:
 
 class MySet(data.Dataset):
 
-    def __init__(self, data_dir, voxel_size, R_range, t_range, select_point_count, noise_strength):
+    def __init__(self, data_dir, voxel_size, R_range, t_range, select_point_count, noise_strength, feature_distance_tresh):
         """
 
         :param data_dir: ply data dir
@@ -31,6 +31,7 @@ class MySet(data.Dataset):
         :param t_range: value range of item of translate vector
         :param select_point_count: point count of one correspondence
         :param noise_strenght: float, specify the noise strenght while add gaussian noise to source pcd
+        :param feature_distance_tresh: feature distance threshold, use for label inlier or outlier
         """
         self.voxel_size = voxel_size
         self.R_range = R_range
@@ -38,25 +39,51 @@ class MySet(data.Dataset):
         self.select_point_count = select_point_count
         self.pcd_pths = [os.path.join(data_dir, i) for i in os.listdir(data_dir)]
         self.noise_strength = noise_strength
+        self.feature_distance_thresh = feature_distance_tresh
 
     def __getitem__(self, index):
         pcd_pth = self.pcd_pths[index]
-        pcd = self.load_one_pcd(pcd_pth)
-        source = copy.deepcopy(pcd)
-        target, R, t_vec = self.random_transform(pcd)  # R * target + t_vec
-        source = self.add_gauss_noise_to_pcd(source)  # add noise to source
-        target_down, target_down_fpfh = self.preprocess_point_cloud(target)
-        source_down, source_down_fpfh = self.preprocess_point_cloud(source)
-        fpfh_regis_result = self.execute_global_registration(source_down, target_down, source_down_fpfh, target_down_fpfh)
-        # print(R)
-        # print(t_vec)
-        # print(fpfh_regis_result.transformation)
-        # print(fpfh_regis_result.correspondence_set)
+        match_indices = np.array([])
+        while match_indices.shape[0] == 0:
+            pcd = self.load_one_pcd(pcd_pth)
+            source = copy.deepcopy(pcd)
+            target, R, t_vec = self.random_transform(pcd)  # R * target + t_vec
+            source = self.add_gauss_noise_to_pcd(source)  # add noise to source
+            target_down, target_down_fpfh = self.preprocess_point_cloud(target)
+            source_down, source_down_fpfh = self.preprocess_point_cloud(source)
+            fpfh_regis_result = self.execute_global_registration(source_down, target_down, source_down_fpfh, target_down_fpfh)
+            match_indices = np.asarray(fpfh_regis_result.correspondence_set)
+        target_down_array = np.asarray(target_down.points)
+        source_down_array = np.asarray(source_down.points)
+        target_down_fpfh_array = np.asarray(target_down_fpfh.data).transpose()
+        source_down_fpfh_array = np.asarray(source_down_fpfh.data).transpose()
+        target_select, source_select, target_fpfh, source_fpfh = self.select_point_feature(target_down_array, source_down_array, target_down_fpfh_array, source_down_fpfh_array, match_indices)
+        feature_dist = self.calc_feature_dist(target_fpfh, source_fpfh)
+        class_label = feature_dist <= self.feature_distance_thresh
+        # tar = o3d.geometry.PointCloud()
+        # sou = o3d.geometry.PointCloud()
+        # tar.points = o3d.utility.Vector3dVector(target_select)
+        # sou.points = o3d.utility.Vector3dVector(source_select)
         # source_down.transform(fpfh_regis_result.transformation)
-        # self.show_pcd([source_down, target_down])
+        # self.show_pcd([sou, tar])
+        d = t.tensor(np.concatenate([source_select, target_select], axis=1)).type(t.FloatTensor)
+        class_label = t.tensor(class_label).type(t.FloatTensor)
+        return d, class_label, target_fpfh, source_fpfh
 
     def __len__(self):
         return len(self.pcd_pths)
+
+    def select_point_feature(self, target_down_array, source_down_array, target_down_fpfh_array, source_down_fpfh_array, match_indices):
+        select_indices = match_indices[rd.choice(np.arange(match_indices.shape[0]), self.select_point_count, replace=True), :]
+        target_select = target_down_array[select_indices[:, 1], :]
+        source_select = source_down_array[select_indices[:, 0], :]
+        target_fpfh = target_down_fpfh_array[select_indices[:, 1], :]
+        source_fpfh = source_down_fpfh_array[select_indices[:, 0], :]
+        return target_select, source_select, target_fpfh, source_fpfh
+
+    def calc_feature_dist(self, target_fpfh, source_fpfh):
+        feature_dist = np.sqrt(np.sum((target_fpfh - source_fpfh) ** 2, axis=1))
+        return feature_dist
 
     def add_gauss_noise_to_pcd(self, pcd):
         point = np.asarray(pcd.points)
@@ -157,11 +184,15 @@ class MySet(data.Dataset):
        return result
 
 
+def find_feature_dist_thresh(pcd_dir, voxel_size, R_range, t_range, select_point_count, noise_strength):
+    s = MySet(pcd_dir, voxel_size, R_range, t_range, select_point_count, noise_strength, 0.1)
+    
+
 if __name__ == "__main__":
-    from copy import deepcopy
     pcd_dir = r"F:\python_project\test_open3d\pcd_dir"
     voxel_size = 0.01
     R_range = [-3.14, 3.14]
     t_range = [-1, 1]
-    s = MySet(pcd_dir, voxel_size, R_range, t_range, 3000, 0.12)
-    s[0]
+    s = MySet(pcd_dir, voxel_size, R_range, t_range, 3000, 0.12, 45)
+    for i in range(100):
+        s[0]
